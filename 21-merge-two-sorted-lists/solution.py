d01ad910391f24ae2ed23fec89eee4d03ee92c79
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2025-12-22 17:55:46 -0500

    problem: merged two sorted lists

---
+++ 21-merge-two-sorted-lists/solution.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from lcutils import ListNode
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        l1, l2 = list1, list2
+        dummy = ListNode()
+        curr = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                curr.next = ListNode(l2.val)
+                l2 = l2.next
+            curr = curr.next
+
+        while l1:
+            curr.next = ListNode(l1.val)
+            curr = curr.next
+            l1 = l1.next
+
+        while l2:
+            curr.next = ListNode(l2.val)
+            curr = curr.next
+            l2 = l2.next
+
+        return dummy.next
+        
+
+
+
+if __name__ == "__main__":
+    # Include one-off tests here or debugging logic that can be run by running this file
+    # e.g. print(solution.two_sum([1, 2, 3, 4], 3))
+    solution = Solution()
